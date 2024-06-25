ec471426bd11e61c04146e0bc726cc9c6eb9b9af
itcrow13 <itcrow13@gmail.com>
2024-06-25 13:12:42 +0300

    -m

--- js/button.js
+++ js/button.js
@@ -1,11 +1,11 @@
+var count = 0;
 document.getElementById("myButton").onclick = function () {
-	var count = 0;
 	count++;
 	if (count % 2 == 0) { 
 		document.getElementById("demo").innerHTML = " ";
 	} else {
 		var img = document.createElement("img");
 	img.src = "image/Watchmen_all.jpg"
-	document.getElementById("myButton").appendChild(img) 
+	document.getElementById("demo").appendChild(img) 
 	}
 }
